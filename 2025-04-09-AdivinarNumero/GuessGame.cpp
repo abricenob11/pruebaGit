dd1bad5b8cf7b8eaf586df064cbe6870e2714f58
Anderson <abricenob@unal.edu.co>
2025-04-09 08:32:49 -0500

    incluir bucle

--- 2025-04-09-AdivinarNumero/GuessGame.cpp
+++ 2025-04-09-AdivinarNumero/GuessGame.cpp
@@ -11,16 +11,19 @@
 int main(void){
     const int NUM = 10;
     int guessed_number;
-    std::cout<< "Adivina un numero entre 1 y 100:\n";
-    std::cin>> guessed_number;
 
-    if(guessed_number == NUM){
-        std::cout << "Ganó!!! ;)\n";
-    } else if(guessed_number > NUM){
-        std::cout << "Te pasaste\n";
-    } else{ // es menor
-        std::cout << "Te falta\n";
+    for(; ; ){ 
+        std::cout<< "Adivina un numero entre 1 y 100:\n";
+        std::cin>> guessed_number;
+
+        if(guessed_number == NUM){
+            std::cout << "Ganó!!! ;)\n";
+            break;
+        } else if(guessed_number > NUM){
+            std::cout << "Te pasaste\n";
+        } else{ // es menor
+            std::cout << "Te falta\n";
+        }
     }
-
     return 0;
 }
