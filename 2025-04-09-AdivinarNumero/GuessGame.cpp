2fb20e8c11138d2dda33a574bba35b4503e4c14f
Anderson <abricenob@unal.edu.co>
2025-04-11 08:07:59 -0500

    validacion rango numero

--- 2025-04-09-AdivinarNumero/GuessGame.cpp
+++ 2025-04-09-AdivinarNumero/GuessGame.cpp
@@ -13,16 +13,25 @@ int main(void){
     int guessed_number = NUM+1;
 
     while(guessed_number != NUM){ 
+        
         std::cout<< "Adivina un numero entre 1 y 100:\n";
         std::cin>> guessed_number;
 
-        if(guessed_number == NUM){
-            std::cout << "Ganó!!! ;)\n";
-        } else if(guessed_number > NUM){
-            std::cout << "Te pasaste\n";
-        } else{ // es menor
-            std::cout << "Te falta\n";
-        }
+            //validación del rango
+
+            if(guessed_number<100 && guessed_number>1){ 
+
+                if(guessed_number == NUM){
+                    std::cout << "Ganó!!! ;)\n";
+                } else if(guessed_number > NUM){
+                    std::cout << "Te pasaste\n";
+                } else{ // es menor
+                    std::cout << "Te falta\n";
+                }
+            }else{
+                std::cout << "Numero en el rango incorrecto\n";
+            }
+
     }
     return 0;
 }
