fea6dbceb6699cf256917003eca75f56155bb07a
Anderson <abricenob@unal.edu.co>
2025-04-14 18:59:58 -0500

    correcion funcion pi_aprox

--- 2025-04-14-PiApproximation/AproximacionDePi.cpp
+++ 2025-04-14-PiApproximation/AproximacionDePi.cpp
@@ -5,7 +5,7 @@ double pi_aprox(int n);
 
 int main (void){
     
-    
+    std::cout<<"Para n: 5 pi es igual a: " << pi_aprox(5) << "\n"; 
     
     return 0;
 }
@@ -14,9 +14,12 @@ int main (void){
 double pi_aprox(int n){
     double pi=0;
     for(int i=0; i<=n; i++){
-        pi += (1/std::pow(16, i))[(4/8*i+1)-(2/8*i+4)-(1/8*i+5)-(1/8*i+6)]
+        pi += (1.0/std::pow(16, i)) * (
+            (4.0 / (8*i+1)) - 
+            (2.0 / (8*i+4)) -
+            (1.0 / (8*i+5)) -
+            (1.0 / (8*i+6)));
     }
-
 
     return pi;
 }
